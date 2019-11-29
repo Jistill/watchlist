9286ccad749e4c995008ded0a2c0e4569fff1a82
Jistill <360999468@qq.com>
2019-11-29 21:15:37 +0800

    update for Uppercase letters of file format

--- app.py
+++ app.py
@@ -4,21 +4,29 @@ import time
 from flask import Flask, request, url_for, send_from_directory
 from werkzeug import secure_filename
 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif','JPG','PNG','JPEG'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.getcwd() + '/uploads'
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 160 * 10240 * 10240
 
 
 html = '''
     <!DOCTYPE html>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <div class="txtCenter">
     <title>裂缝检测Demo</title>
-    <h1>裂缝检测Demo</h1>
+    <h1>裂缝检测Demo</h1>    
     <form method=post enctype=multipart/form-data>
-         <input type=file name=file>
-         <input type=submit value=上传>
+         <input type=file name=file > 
+         <input type=submit value=预测>
     </form>
+    </div>
+    <style>  
+.txtCenter{
+    text-align:center;  
+} 
+</style>
     '''
 
 
@@ -44,8 +52,8 @@ def upload_file():
             file_url = url_for('uploaded_file', filename=filename)
             area = 'uploads/' + filename
             re = net.get_label(area)
-            results = '<br><h2>' + re + '<br>' + filename +'<h2>'
-            return html + '<br><img src=' + file_url + ' height="400">' + results 
+            results = '<br><h2>' + re +'</h2>'
+            return html + '<div class="txtCenter"><br><img src=' + file_url + ' height="400">' + results + '</div>'
     return html
 
 
